e65d18e85cc3189231441824470b1ed08c592d69
amancera <amancera@IN-UITS-CSLT027>
2018-06-22 13:59:37 -0400

    Implemented search button

--- IUERM-RSS/IUERM-RRS/Controllers/HomeController.cs
+++ IUERM-RSS/IUERM-RRS/Controllers/HomeController.cs
@@ -26,20 +26,58 @@ namespace IUERM_RRS.Controllers
             ScheduleViewModel.SetColumns(mainRepository.GetColumnsConfig());
             ScheduleViewModel.Checked = false;
             ViewBag.Checked = false;
+            ViewBag.SearchValue = string.Empty;
             return View();
         }
 
-        public ActionResult AdvancedView(string check)
+        public ActionResult AdvancedView(string check, string searchValue)
         {
             ScheduleViewModel.SetColumns(mainRepository.GetColumnsConfig());
             ScheduleViewModel.Checked = Convert.ToBoolean(check);
             ViewBag.Checked = Convert.ToBoolean(check);
+            ViewBag.SearchValue = searchValue;
             return View("Index");
         }
 
-        public ActionResult Schedules_Read([DataSourceRequest]DataSourceRequest request)
+        public ActionResult Schedules_Read([DataSourceRequest]DataSourceRequest request, string searchValue)
         {
             List<ScheduleViewModel> schedules = scheduleRepository.GetAllRecords();
+            if (!string.IsNullOrWhiteSpace(searchValue))
+            {
+                schedules = schedules.Where(s =>
+                {
+                    bool valid = false;
+
+                    if ((string.Empty + s.SCH_AreaScope).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Coordinator).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_CreationDate2).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Description).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Disposition).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_EventCode).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_GoverningPolicy).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_GoverningRegulation).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_GoverningStatute).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_IsActive).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_IsVital).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Office).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Reason).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_RecordMedium).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_RecordSeries).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_RecordSeriesCode).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_RequiresDestruction).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Retainer).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Retention).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_RetentionArea).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_RetentionAreaDescription).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_RetentionSubArea).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_StewardDomain).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Type).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+                    if ((string.Empty + s.SCH_Years).ToUpper().Contains(searchValue.ToUpper())) valid = true;
+
+                    return valid;
+
+                }).ToList();
+            }
             DataSourceResult result = schedules.AsQueryable().ToDataSourceResult(request);
             return Json(result, JsonRequestBehavior.AllowGet);
         }
